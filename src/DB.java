ec6532726761ccfc04357ff8a7c43e6df04ca36f
Manel Zengouri <manel95zengouri@gmail.com>
2021-01-10 17:53:02 +0100

    Third Commit

--- src/DB.java
+++ src/DB.java
@@ -3,12 +3,12 @@ import java.sql.DriverManager;
 
 public class DB {
 	public static Connection getConnection(){
-		Connection con=null;
+		Connection conn=null;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","");
 		}catch(Exception e){System.out.println(e);}
-		return con;
+		return conn;
 	}
 
 }
